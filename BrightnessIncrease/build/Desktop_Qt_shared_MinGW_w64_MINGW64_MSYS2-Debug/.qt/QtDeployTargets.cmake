6460582361cc7f247e1de56d39ccd9acb4ffb307
Владислав Саттаров <75261104+VladislavSattarov@users.noreply.github.com>
2025-07-04 15:49:57 +0300

    Delete BrightnessIncrease/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug directory

--- BrightnessIncrease/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_BrightnessIncrease_FILE C:/Users/vlads/Documents/Brightness-Increase/BrightnessIncrease/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/BrightnessIncrease.exe)
-set(__QT_DEPLOY_TARGET_BrightnessIncrease_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_BrightnessIncrease_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
